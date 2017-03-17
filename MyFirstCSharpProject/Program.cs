08e646cfd25e5214a5660cbc47021b6384874fce
bajaderkizcynaderek <dz@MacBook-Pro-dz.local>
2017-03-17 19:48:56 +0100

    omatko, ale mi sie chce kaszlec

--- MyFirstCSharpProject/Program.cs
+++ MyFirstCSharpProject/Program.cs
@@ -7,8 +7,19 @@ namespace MyFirstCSharpProject
 		public static void Main(string[] args) // fuck my args
 
 		{
-			var name = "Beeelzebuub Gorgoroth";
-			Console.WriteLine($"Hello {name}");
+			string string1 = "  lorem Ipsum dolor amet sit    ";
+			string string2 = " concecTuer aDAm *chomik* grzyb ";
+			string trimm = string1.Trim();
+			string szesc = string1.Trim().Substring(0, 6);
+
+			Console.WriteLine($"Hello {string1}");
+			Console.WriteLine (string1.ToUpper() + string2.ToLower());
+			Console.WriteLine(trimm);
+			Console.WriteLine (string1.Length+trimm.Length);
+			Console.WriteLine ($"urywkiem tekstu jest-{szesc}-");
+
+			//jestem glupi lol.
+
 		}
 	}
 }
